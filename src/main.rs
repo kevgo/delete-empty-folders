d051e4644d22053fe52972a071d4efed0352f3d1
Kevin Goslar <kevin.goslar@gmail.com>
2026-05-07 20:44:29 -0500

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ fn main() -> io::Result<()> {
 pub fn remove_empty_descendants(dir: &Path, root: &Path) -> io::Result<bool> {
     let entries = fs::read_dir(dir)?;
     let mut has_children = false;
+    let mut child_dirs = Vec::new();
     for entry in entries {
         let entry = entry?;
         let Ok(filetype) = entry.file_type() else {
@@ -32,6 +33,11 @@ pub fn remove_empty_descendants(dir: &Path, root: &Path) -> io::Result<bool> {
             has_children = true;
             continue;
         }
+        child_dirs.push(path);
+    }
+    // Note: sort the child directories because certain file systems return entries in random order.
+    child_dirs.sort();
+    for path in child_dirs {
         let gone_now = remove_empty_descendants(&path, root)?;
         if !gone_now {
             has_children = true;
