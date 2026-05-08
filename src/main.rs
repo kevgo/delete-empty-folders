2eb3ff72e309f71d8f03fcf468b80e444932cf5d
Kevin Goslar <kevin.goslar@gmail.com>
2026-05-08 06:21:14 -0500

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-/// Simpler type signature for internal use.
+/// simpler type signature for internal use
 type Gitignore<'a> = Option<&'a gitignore::File<'a>>;
 
 /// Deletes empty directories under `dir`, depth-first. Never removes `root` itself.
